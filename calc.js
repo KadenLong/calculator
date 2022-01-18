8cb2d004653a56f597567ad7ace61334ac4c96f1
Kaden Long <kglong20@gmail.com>
2022-01-18 15:03:02 -0700

    Division implementaion working

--- calc.js
+++ calc.js
@@ -27,9 +27,9 @@ reader.question("What would you like to calculate? ", function(input){
 	 else if(mathSymbol === 'x' || mathSymbol === '*'){
 		console.log(num1 * num2);
 	} 
-	//else if(mathSymbol === '/'){
-	//	console.log(num1 / num2);
-	//}
+	else if(mathSymbol === '/'){
+		console.log(num1 / num2);
+	}
 	// else if (mathSymbol === 'sqrt'){
 	//	console.log(Math.sqrt(num1));
 	//}
