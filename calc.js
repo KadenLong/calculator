fa2cd5ade7620e3ee7c30e944c17cb4ecab345ff
Kaden Long <kglong20@gmail.com>
2022-01-18 15:04:00 -0700

    square root implementaion working, using user imput: sqrt

--- calc.js
+++ calc.js
@@ -30,9 +30,9 @@ reader.question("What would you like to calculate? ", function(input){
 	else if(mathSymbol === '/'){
 		console.log(num1 / num2);
 	}
-	// else if (mathSymbol === 'sqrt'){
-	//	console.log(Math.sqrt(num1));
-	//}
+	else if (mathSymbol === 'sqrt'){
+		console.log(Math.sqrt(num1));
+	}
 	//else{console.log("Error, not a valid math sysmbol!")};
 		
 
